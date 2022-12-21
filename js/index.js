5abc16f3e7e6ea225e697cbd5098960a149b8ffa
SEONG_WON <kimsungwon57@naver.com>
2022-12-21 17:36:56 +0900

    1221 프로젝트 코딩

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
